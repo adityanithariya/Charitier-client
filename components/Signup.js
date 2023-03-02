878ed5c5f7650873067e0f325b212ab0f71a2dfb
Aditya Gupta <ad13002ag@gmail.com>
2023-03-03 00:26:14 +0530

    initial-signup-page

--- components/Signup.js
+++ components/Signup.js
@@ -3,32 +3,160 @@ import styles from "@css/Signup.module.css";
 
 const Signup = () => {
     return (
-        <div>
-            <form className={styles.form}>
-                <input type="text" placeholder="Name of NGO" />
-                <input type="email" placeholder="Email" />
-                <input type="url" placeholder="Website Url" />
-                <input type="file" placeholder="Registration Certificate" />
-                <input type="text" placeholder="Act Name" />
-                <input type="text" placeholder="Type of NGO" /> {/* Can be a dropdown*/}
-                <input type="file" placeholder="Pan Card" />
-                <input type="text" placeholder="key issues" />
-                <div>
-                    <div>FCRA Details</div>
-                    <input type="checkbox" id="isAvailable" />
-                    <label htmlFor="isAvailable">Is Available</label>
-                    <input type="number" placeholder="Registration No" />
-                </div>
-                <div>
-                    <input type="text" placeholder="Place" />
-                    <input type="text" placeholder="City" />
-                    <input type="text" placeholder="State" />
-                    <input type="number" placeholder="Pincode" />
-                    <input type="number" placeholder="Phone Number" />
-                    <input type="number" placeholder="Alt Phone Number" />
-                </div>
-            </form>
-        </div>
+        <>
+            <h2 className={styles.heading}>Sign-up</h2>
+            <div className="align-center">
+                <form className={styles.form}>
+                    <div className={styles.ele}>
+                        <label htmlFor="ngo-name">Name of ngo</label>
+                        <input
+                            type="text"
+                            placeholder="Name of NGO"
+                            id="ngo-name"
+                        />
+                    </div>
+                    <div className={styles.ele}>
+                        <label htmlFor="ngo-email">E-mail</label>
+                        <input
+                            type="email"
+                            id="ngo-email"
+                            placeholder="Email"
+                        />
+                    </div>
+                    <div className={styles.ele}>
+                        <label htmlFor="ngo-web-url">Wbsite URL</label>
+                        <input
+                            type="url"
+                            placeholder="Website Url"
+                            id="ngo-web-url"
+                        />
+                    </div>
+                    <div className={styles.ele}>
+                        <label htmlFor="ngo-certificate">
+                            Registration Certificate
+                        </label>
+                        <input
+                            type="file"
+                            placeholder="Registration Certificate"
+                            id="ngo-certificate"
+                        />
+                    </div>
+                    <div className={styles.ele}>
+                        <label htmlFor="ngo-Act-Name">Account Name</label>
+                        <input
+                            type="text"
+                            placeholder="Act Name"
+                            id="ngo-Act-Name"
+                        />
+                    </div>
+                    <div className={styles.ele}>
+                        <label htmlFor="ngo-type">Type of Ngo</label>
+                        <input
+                            type="text"
+                            placeholder="Type of NGO"
+                            id="ngo-type"
+                        />{" "}
+                    </div>
+                    {/* Can be a dropdown*/}
+                    <div className={styles.ele}>
+                        <label htmlFor="ngo-pan">Pan Card</label>
+                        <input
+                            type="file"
+                            placeholder="Pan Card"
+                            id="ngo-pan"
+                        />
+                    </div>
+                    <div className={styles.ele}>
+                        <label htmlFor="ngo-key">key issues</label>
+
+                        <input
+                            type="text"
+                            placeholder="key issues"
+                            id="ngo-key"
+                        />
+                    </div>
+                    <div>
+                        <div>FCRA Details</div>
+                        <div className={styles.ele}>
+                            <input type="checkbox" id="isAvailable" />
+                            <label htmlFor="isAvailable">Is Available</label>
+                        </div>
+                        <div className={styles.ele}>
+                            <label htmlFor="ngo-Registration-No">
+                                Registration No
+                            </label>
+
+                            <input
+                                type="number"
+                                placeholder="Registration No"
+                                id="ngo-Registration-No"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <div className={styles.ele}>
+                            <label htmlFor="ngo-place">Place</label>
+
+                            <input
+                                type="text"
+                                placeholder="Place"
+                                id="ngo-place"
+                            />
+                        </div>
+                        <div className={styles.ele}>
+                            <label htmlFor="ngo-city">City</label>
+
+                            <input
+                                type="text"
+                                placeholder="City"
+                                id="ngo-city"
+                            />
+                        </div>
+                        <div className={styles.ele}>
+                            <label htmlFor="ngo-state">State</label>
+
+                            <input
+                                type="text"
+                                placeholder="State"
+                                id="ngo-state"
+                            />
+                        </div>
+                        <div className={styles.ele}>
+                            <label htmlFor="ngo-pincode">Name of ngo</label>
+
+                            <input
+                                type="number"
+                                placeholder="Pincode"
+                                id="ngo-pincode"
+                            />
+                        </div>
+                        <div className={styles.ele}>
+                            <label htmlFor="ngo-number">Number</label>
+
+                            <input
+                                type="number"
+                                placeholder="Phone Number"
+                                id="ngo-number"
+                            />
+                        </div>
+                        <div className={styles.ele}>
+                            <label htmlFor="ngo-alt-number">
+                                AlterateNumber
+                            </label>
+
+                            <input
+                                type="number"
+                                placeholder="Phone Number"
+                                id="ngo-alt-number"
+                            />
+                        </div>
+
+                        {/* <input type="number" placeholder="Alt Phone Number" /> */}
+                    </div>
+                </form>
+            </div>
+            <button className={styles.btn}>Submit</button>
+        </>
     );
 };
 
