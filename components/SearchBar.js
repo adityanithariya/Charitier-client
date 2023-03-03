1f629370a6b9484aaea3907f6923e47811cf27cf
Abhishek Sharma <abhisheksharma620703@gmail.com>
2023-03-04 01:03:19 +0530

    search bar fix

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,13 +1,53 @@
-import React from 'react'
+import { useState } from "react";
+import "./SearchBar.css";
+var data = require("./MOCK_DATA.json");
 
+export default function App() {
+  const [value, setValue] = useState("");
 
-export default function SearchBar() {
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onSearch = (searchTerm) => {
+    setValue(searchTerm);
+    // our api to fetch the search result
+    console.log("search ", searchTerm);
+  };
+
   return (
-        <div className="container" >
-           <form className='d-flex'>
-            <input className='form-control me 2' type="search"  placeholder='Search' />
-           <button className='btn btn-outline-info' type='send' > Search</button>
-           </form>
+    <div className="App">
+      
+
+      <div className="search-container">
+        <div className="search-inner">
+          <input type="text" value={value} onChange={onChange} />
+          <button onClick={() => onSearch(value)}> Search </button>
         </div>
-  )
+        <div className="dropdown">
+          {data
+            .filter((item) => {
+              const searchTerm = value.toLowerCase();
+              const fullName = item.full_name.toLowerCase();
+
+              return (
+                searchTerm &&
+                fullName.startsWith(searchTerm) &&
+                fullName !== searchTerm
+              );
+            })
+            .slice(0, 10)
+            .map((item) => (
+              <div
+                onClick={() => onSearch(item.full_name)}
+                className="dropdown-row"
+                key={item.full_name}
+              >
+                {item.full_name}
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/database.js
+++ components/database.js
@@ -1,28 +1,28 @@
 export const Leaderboard = [
     {
-        name: "Shawn Hanna",
-        location: "India",
+        name: "Aditya soni",
+        location: "patna",
         score : 1550,
         img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
         dt: "2022-02-10"
     },
     {
-        name: "Fidel Hand",
-        location: "USA",
+        name: "Aditya Gupta",
+        location: "Jaipur",
         score : 2310,
         img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
         dt: "2021-01-01"
     },
     {
-        name: "Bessie Hickle",
-        location: "Chaina",
+        name: "Abhishek Sharma",
+        location: "Chittorgarh",
         score : 350,
         img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
         dt: "2021-08-17"
     },
     {
-        name: "Adella Wunsch",
-        location: "Japan",
+        name: "Prashant Singh",
+        location: "Banaras",
         score : 2100,
         img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
         dt: "2021-10-23"
