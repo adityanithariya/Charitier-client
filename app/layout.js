f273723da3d69bd30c24e81022595fc9b492dc3d
Aditya Gupta <ad13002ag@gmail.com>
2023-03-03 17:10:21 +0530

    Bug Fixes

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
                     href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
                     rel="stylesheet"
                     integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
-                    crossorigin="anonymous"
+                    crossOrigin="anonymous"
                 />
             </head>
             <body>
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
                 <script
                     src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
                     integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-                    crossorigin="anonymous"
+                    crossOrigin="anonymous"
                 ></script>
             </body>
         </html>
